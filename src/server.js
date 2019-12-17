5568f49037727787f02205bb39ac79ba19bd3eae
antoniojunioalves <antoniojunioalves@gmail.com>
2019-12-17 13:51:37 -0300

    Corrigindo o script de start

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import app from './app';
+// const app = require('./app');
 const port = process.env.PORT || 3030;
 
 app.listen(port || 3030, () => {
-  console.log("Rodando na porta: ", port);
+  console.log('Rodando na porta: ', port);
 });
